f4fd33a3175e35936ee90177b7ee7dff8c7be674
Jaobuenoh <joaobuenoh@hotmail.com>
2024-03-06 22:37:34 -0300

    feat: added incomplete button press animation

--- JS Drum/script.js
+++ JS Drum/script.js
@@ -1,10 +1,16 @@
-window.addEventListener('keydown', function(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    if(!audio){
-     return;
-    }else
-    audio.currentTime = 0;
-    audio.play();
-    console.log(audio);
-    
+window.addEventListener("keydown", function (e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  if (!audio || !key) {
+    return;
+  }
+
+  key.classList.add("playing");
+
+  
+
+  audio.currentTime = 0;
+  audio.play();
+  console.log(audio);
 });
